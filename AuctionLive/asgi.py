08101647bc0ccc115ffde3122f730dc5309056b3
mirmohsen <mirmohsen@gmail.com>
2025-10-31 09:40:13 +0330

    feat: add env.example and added Dockerfile for production

--- AuctionLive/asgi.py
+++ AuctionLive/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for resa project.
+ASGI config for auction project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resa.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AuctionLive.settings")
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
